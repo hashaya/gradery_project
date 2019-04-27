75385c372c01ecf70c1d73176dcb99745d55f99b
smhash78 <49486792+smhash78@users.noreply.github.com>
2019-04-27 14:19:24 +0430

    Update basic.c

--- basic.c
+++ basic.c
@@ -1,3 +1,20 @@
+int get_number();
+
+int randint(int upper,int lower); // Generates a random integer between upper and lower
+
+void make(char board[], char what, int n); // Puts n number of a given symbol in empty spaces within the board
+
+void addpoint(char who,int increase);
+
+// Three following functions are used to make and print game board
+
+void row_maker(char board[], int length, int start, char sth); // Makes a row of sths started at block start in borad as long as length (Just called in board_maker())
+
+void board_maker(char board[]); // The main board maker which is called at the first of the game
+
+void print_screen(char board[]); // Prints the game board with all stuff
+
+
 int get_number(){
     int num,c;
     num=0;
@@ -6,7 +23,7 @@ int get_number(){
     return num;
 }
 
-int randint(int upper,int lower){ //generates a random integer between upper and lower
+int randint(int upper,int lower){
     int num;
     num=(rand()%(upper-lower+1))+lower;
     return num;
@@ -25,3 +42,42 @@ void make(char board[], char what, int n){ //puts n number of a given symbol in 
 void addpoint(char who,int increase){
 
 }
+
+void row_maker(char board[], int length, int start, char sth) {
+    int i = 0;
+    while (i < length) {
+        board[start + i] = sth;
+        i++;
+    }
+}
+
+void board_maker(char board[]) {
+    row_maker(board, x + 2, 0, '#');
+    int i = 0;
+    int pointer = x + 2;
+    while (i < y) {
+        board[pointer] = '|';
+        pointer++;
+        row_maker(board, x, pointer, ' ');
+        pointer = pointer + x;
+        board[pointer] = '|';
+        pointer++;
+        i++;
+    }
+    row_maker(board, x + 2, pointer, '#');
+    pointer = pointer + x + 3;
+    board[pointer] = '\0';
+}
+
+void print_screen(char board[]) {
+    int i, j;
+    i = 0;
+    j = 0;
+    while (board[i] != '\0') {
+        putchar(board[i]);
+        i++;
+        if (i%(x + 2) == 0)
+            putchar('\n');
+    }
+}
+
