54bdfd0332723758b4b2d35a4ebac0884e883161
smhash78 <49486792+smhash78@users.noreply.github.com>
2019-04-27 13:43:28 +0430

    Update basic.c

--- basic.c
+++ basic.c
@@ -12,10 +12,14 @@ int randint(int upper,int lower){ //generates a random integer between upper and
     return num;
 }
 
-void make(char board[],char what){ //puts a given symbol in an empty space within the board
-    int place;
+void make(char board[], char what, int n){ //puts n number of a given symbol in empty spaces within the board
+    int place, i;
+    i = 0;
+    while (i < n) {
     for(place=randint((x*y)-(x+1), (x+1));board[place]!=' ';place=randint((x*y)-(x+1), (x+1)));
     board[place]=what;
+    i++;
+    }
 }
 
 void addpoint(char who,int increase){
