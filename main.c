2a2e16316fa9afc3da6ecc49b59999e3310fbe00
smhash78 <49486792+smhash78@users.noreply.github.com>
2019-05-10 15:40:11 +0430

    Update main.c

--- main.c
+++ main.c
@@ -1,54 +1,67 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include "move.h"
 
 #define XSIGN '!'
 #define YSIGN '?'
 
 int mode,temp_time,x_point,y_point,x,y,x_direction,y_direction;
 float game_time;
-char moves[201],board[10000];
+char moves[200],board[10000];
 int main(void){
     char c;
     int i;
-    game_time=60.0;
-    printf(""); // First game message
-    mode = get_number();
+    game_time = 60.0;
+    x_point = 0;
+    y_point = 0;
+    printf("\n Hello\n\n Choose your game mode:\n\n 0: Player1 vs Player2\n 1: Player1 vs Computer\n"); // First game message
+    mode = getchar();
     system("cls");
-    printf(""); // Game guide
-    print_screen();
-    if (mode == 0) {
-        while(time != 0){ // Main loop for two player mode
-                i=0;
+    if (mode != 1) {
+        printf("\n Player 1: W(up), S(down), D(right), A(left)\n Player 2: I(up), K(down), L(right), J(left)\n\n Q(quit the game)\n\n Press Enter to enter the game...");
+        board_maker(board);
+        getchar();
+        print_screen();
+        while(game_time != 0){ // Main loop for two player mode
                 temp_time = clock();
+                i = 0;
+                // get start point
                 if(_kbhit()&&i<=200){
                     c=_getch();
                     moves[i]=c;
                     i++;
                 }
-                moves[i]='\n';
+                moves[i]='\0';
+                // get end point
                 movep(XSIGN, moves, board);
                 movep(YSIGN, moves, board);
                 game_time = game_time - 0.2;
                 while (clock() < temp_time + 0.2);
+                system("cls");
                 print_screen();
         }
     }
-    if (mode == 1) {
-        while(time != 0){ // Main loop for AI
+    else {
+        printf("\n Player 1: W(up), S(down), D(right), A(left)\n\n Q(quit the game)\n\n Press Enter to enter the game...");
+        board_maker(board);
+        getchar();
+        print_screen();
+        while(game_time != 0){ // Main loop for AI
                 temp_time = clock();
-                i=0;
+                // get start point
+                i = 0;
                 if(_kbhit()&&i<=200){
                     c=_getch();
                     moves[i]=c;
                     i++;
                 }
-                moves[i]='\n';
+                moves[i]='\0';
+                // get end point
                 movep(XSIGN, moves, board);
                 ai(board);
                 game_time = game_time - 0.2;
                 while (clock() < temp_time + 0.2);
+                system("cls");
                 print_screen();
         }
     }
