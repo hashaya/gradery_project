dff9a39622c9ca005b9465795534c81b378a0141
smhash78 <49486792+smhash78@users.noreply.github.com>
2019-05-11 20:54:22 +0430

    Update main.c

--- main.c
+++ main.c
@@ -5,43 +5,48 @@
 #define XSIGN '!'
 #define YSIGN '?'
 
-int mode,temp_time,x_point,y_point,x,y,x_direction,y_direction;
+int mode,temp_time,x_point = 0,y_point = 0,x = 100,y = 30,x_direction,y_direction;
 float game_time;
-char moves[200],board[10000];
+char moves[200], board[10000];
 int main(void){
     char c;
     int i;
-    game_time = 60.0;
-    x_point = 0;
-    y_point = 0;
+    game_time = 5;
     printf("\n Hello\n\n Choose your game mode:\n\n 0: Player1 vs Player2\n 1: Player1 vs Computer\n"); // First game message
-    mode = getchar();
+    mode = get_number();
     system("cls");
-    if (mode != 1) {
+    if (mode == 0) {
         printf("\n Player 1: W(up), S(down), D(right), A(left)\n Player 2: I(up), K(down), L(right), J(left)\n\n Q(quit the game)\n\n Press Enter to enter the game...");
         board_maker(board);
         getchar();
+        make(board, 'B', 10);
+        make(board, 'T', 2);
+        make(board, 'F', 1);
+        make(board, XSIGN, 1);
+        make(board, YSIGN, 1);
         print_screen();
-        while(game_time != 0){ // Main loop for two player mode
+        getchar();
+        while(game_time >= 0){ // Main loop for two player mode
                 temp_time = clock();
                 i = 0;
                 // get start point
-                if(_kbhit()&&i<=200){
-                    c=_getch();
-                    moves[i]=c;
-                    i++;
+                while (clock() < temp_time + 200) {
+                    if(_kbhit()&&i<=200){
+                        c=_getch();
+                        moves[i]=c;
+                        i++;
+                    }
                 }
                 moves[i]='\0';
                 // get end point
                 movep(XSIGN, moves, board);
                 movep(YSIGN, moves, board);
                 game_time = game_time - 0.2;
-                while (clock() < temp_time + 0.2);
                 system("cls");
                 print_screen();
         }
     }
-    else {
+    else if (mode != 0){
         printf("\n Player 1: W(up), S(down), D(right), A(left)\n\n Q(quit the game)\n\n Press Enter to enter the game...");
         board_maker(board);
         getchar();
