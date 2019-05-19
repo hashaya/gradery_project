473102d089d6072bcbc92267ad2d31aff35e5cd4
smhash78 <49486792+smhash78@users.noreply.github.com>
2019-05-19 16:32:58 +0430

    Update main.c

--- main.c
+++ main.c
@@ -5,36 +5,37 @@
 #define XSIGN '!'
 #define YSIGN '?'
 
-int mode,temp_time,x_point = 0,y_point = 0,x = 100,y = 30,x_direction,y_direction;
+int mode,temp_time,x_point = 0,y_point = 0,x = 60,y = 20,x_direction,y_direction, x_loc, y_loc;
 float game_time;
-char moves[200], board[10000];
+char moves[10], board[12000];
 int main(void){
     char c;
     int i;
     game_time = 10;
-    printf("Hello\n\n Choose your game mode:\n\n 0: Player1 vs Player2\n 1: Player1 vs Computer\n"); // First game message
+    printf("\n Hello\n\n Choose your game mode:\n\n 1: Player1 vs Player2\n 2: Player1 vs Computer\n 3: Magical mode (Two Players)\n\n 0: Advanced options\n"); // First game message
     mode = get_number();
     system("cls");
-    if (mode == 0) {
+    if (mode == 1) {
         printf("\n Player 1: W(up), S(down), D(right), A(left)\n Player 2: I(up), K(down), L(right), J(left)\n\n Q(quit the game)\n\n Press Enter to enter the game...");
         board_maker(board);
         getchar();
         make(board, 'B', 10);
         make(board, 'T', 2);
         make(board, 'F', 1);
-        make(board, '.', 700);
-        make(board, XSIGN, 1);
+        make(board, '.', 100);
+        make(board, XSIGN, 3);
         make(board, YSIGN, 1);
-        print_screen();
+        system("cls");
+        print_screen(board);
         getchar();
-        while(game_time >= 0){ // Main loop for two player mode
+        while(game_time > 0.1){ // Main loop for two player mode
                 temp_time = clock();
                 i = 0;
                 // get start point
                 while (clock() < temp_time + 200) {
-                    if(_kbhit()&&i<=200){
-                        c=_getch();
-                        moves[i]=c;
+                    if(_kbhit() && i < 10){
+                        c = _getch();
+                        moves[i] = c;
                         i++;
                     }
                 }
@@ -43,12 +44,13 @@ int main(void){
                 movep(XSIGN, moves, board);
                 movep(YSIGN, moves, board);
                 game_time = game_time - 0.2;
-                while (clock() < temp_time + 0.2);
+                //while (clock() < temp_time + 0.2);
                 system("cls");
                 print_screen(board);
+                print_score();
         }
     }
-    else if (mode == 1){
+    else if (mode == 2){
         printf("\n Player 1: W(up), S(down), D(right), A(left)\n\n Q(quit the game)\n\n Press Enter to enter the game...");
         board_maker(board);
         getchar();
@@ -70,6 +72,12 @@ int main(void){
                 system("cls");
                 print_screen(board);
         }
+    }
+    else if (mode == 3) {
+
+    }
+    else if (mode == 0) {
+
     }
     else{
         printf("Mode not applicable, try again.\n");
@@ -94,5 +102,4 @@ int main(void){
             main();
         }
     }
-    return 0;
 }
