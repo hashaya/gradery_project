2a43163250ec0c3c65f5f92ecdcb474649305b91
smhash78 <49486792+smhash78@users.noreply.github.com>
2019-05-19 16:32:30 +0430

    Update move.c

--- move.c
+++ move.c
@@ -1,98 +1,78 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
+// to include necessary global variables
+extern int x, y, x_direction, y_direction, x_loc, y_loc;
 #define XSIGN '!'
 #define YSIGN '?'
 
-int mode,temp_time,x_point = 0,y_point = 0,x = 100,y = 30,x_direction,y_direction;
-float game_time;
-char moves[200], board[10000];
-int main(void){
-    char c;
+// declarations
+int findindex(char board[], char target);
+
+void teleport(char board[], char what);
+
+void changeindex(char board[],int from,int to);
+
+void movep(char player_sign, char control[],char board[]);
+
+
+int findindex(char board[], char target){
     int i;
-    game_time = 10;
-    printf("Hello\n\n Choose your game mode:\n\n 0: Player1 vs Player2\n 1: Player1 vs Computer\n"); // First game message
-    mode = get_number();
-    system("cls");
-    if (mode == 0) {
-        printf("\n Player 1: W(up), S(down), D(right), A(left)\n Player 2: I(up), K(down), L(right), J(left)\n\n Q(quit the game)\n\n Press Enter to enter the game...");
-        board_maker(board);
-        getchar();
-        make(board, 'B', 10);
-        make(board, 'T', 2);
-        make(board, 'F', 1);
-        make(board, '.', 700);
-        make(board, XSIGN, 1);
-        make(board, YSIGN, 1);
-        print_screen();
-        getchar();
-        while(game_time >= 0){ // Main loop for two player mode
-                temp_time = clock();
-                i = 0;
-                // get start point
-                while (clock() < temp_time + 200) {
-                    if(_kbhit()&&i<=200){
-                        c=_getch();
-                        moves[i]=c;
-                        i++;
-                    }
-                }
-                moves[i]='\0';
-                // get end point
-                movep(XSIGN, moves, board);
-                movep(YSIGN, moves, board);
-                game_time = game_time - 0.2;
-                while (clock() < temp_time + 0.2);
-                system("cls");
-                print_screen(board);
+    for(i=0; board[i] != target; i++){
+        if (board[i] == '\0')
+            return -1; //error 404: not found
+    }
+    return i;
+}
+
+void teleport(char board[], char what){
+    int to=findindex(board,'T');
+    int from=findindex(board,what);
+    board[from]=' ';
+    board[to]=what;
+}
+
+void changeindex(char board[],int from,int to){
+    char what;
+    if (to == 'd' || to == 'l')
+        to = from + 1;
+    else if (to == 'w' || to == 'i')
+        to = from - x - 2;
+    else if (to == 'a' || to == 'j')
+        to = from - 1;
+    else if (to == 's' || to == 'k')
+        to = from + x + 2;
+
+    if(board[to]!='B'){
+        what=board[from];
+        board[from]=' ';
+        if(board[to]=='.')
+            addpoint(what,1);
+        else if(board[to]=='F')
+            addpoint(what,5);
+        board[to]=what;
+        if(board[to]=='T'){
+            teleport(board,what);
         }
+        if (what == XSIGN)
+            x_loc = to;
+        else if (what == YSIGN)
+            y_loc = to;
     }
-    else if (mode == 1){
-        printf("\n Player 1: W(up), S(down), D(right), A(left)\n\n Q(quit the game)\n\n Press Enter to enter the game...");
-        board_maker(board);
-        getchar();
-        print_screen();
-        while(game_time != 0){ // Main loop for AI
-                temp_time = clock();
-                // get start point
-                i = 0;
-                if(_kbhit()&&i<=200){
-                    c=_getch();
-                    moves[i]=c;
-                    i++;
-                }
-                moves[i]='\0';
-                // get end point
-                movep(XSIGN, moves, board);
-                //ai(board);
-                game_time = game_time - 0.2;
-                system("cls");
-                print_screen(board);
+}
+
+void movep(char player_sign, char control[],char board[]){
+    int i, index;
+    if (player_sign == XSIGN) {
+        for(i = 0; control[i] != '\0'; i++){
+            if (control[i] == 'd' || control[i] == 'w' || control[i] == 'a' || control[i] == 's')
+                x_direction = control[i];
         }
+        changeindex(board, x_loc, x_direction);
     }
-    else{
-        printf("Mode not applicable, try again");
-        main();
-    }
-    system("cls");
-    if (x_point > y_point){
-        printf("Player 1 won the game!\n");
-    }
-    if (x_point < y_point){
-        printf("Player 2 won the game!\n");
-    }
-    if (x_point == y_point){
-        printf("Draw!\n");
-    }
-    printf("\nPress q to quit or r to play again.\n");
-    while((c=_getch())!='q'||c!='r'){
-        if(c=='q')
-            return 0;
-        if(c=='r'){
-            system("cls");
-            main();
+
+    else if (player_sign == YSIGN) {
+        for(i = 0; control[i] != '\0'; i++){
+            if (control[i] == 'l' || control[i] == 'i' || control[i] == 'j' || control[i] == 'k')
+                y_direction = control[i];
         }
+        changeindex(board, y_loc, y_direction);
     }
-    return 0;
 }
